55822c4d4a7302dc37cdba31ff30020d8c69d5a7
Nicolas Winkler <nicolas.winkler@gmx.ch>
2020-05-10 00:14:03 +0200

    test for style

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QPixmap>
 #include <QScreen>
 #include <QSplashScreen>
+#include <QStyleFactory>
 #include <cmath>
 
 int main(int argc, char *argv[])
@@ -18,6 +19,18 @@ int main(int argc, char *argv[])
     QSplashScreen splash{ splashScaled };
     a.processEvents();
     splash.show();
+
+    auto list = QStyleFactory::keys();
+    for (auto& str : list)
+        printf("key: %s\n", str.toStdString().c_str());
+    //a.setStyle(QStyleFactory::create("plastique"));
+    /*QPalette p = qApp->palette();
+    p.setColor(QPalette::Window, QColor(53,53,53));
+    p.setColor(QPalette::Button, QColor(53,53,53));
+    p.setColor(QPalette::Highlight, QColor(142,45,197));
+    p.setColor(QPalette::ButtonText, QColor(255,255,255));
+    p.setColor(QPalette::WindowText, QColor(255,255,255));
+    qApp->setPalette(p);*/
     Almond w;
     splash.finish(&w);
     a.processEvents();
